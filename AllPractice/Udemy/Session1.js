dbea438a33c4ec808d2b9013f58b007cb9294386
Ota-creater <bsyota18@gmail.com>
2026-09-04 17:33:23 +0900

    作業用フォルダ、自習用・Udemy用で作成

---
+++ AllPractice/Udemy/Session1.js
@@ -0,0 +1,15 @@
+"use strict";
+let hasValue = true;
+let count = 10;
+let float = 3.14;
+let negative = -0.12;
+let single = 'hello';
+let double = "hello";
+let back = `hello`;
+//型推論
+let one = 1;
+const person = {
+    name: "Jack",
+    age: 21
+};
+console.log(person.name);
