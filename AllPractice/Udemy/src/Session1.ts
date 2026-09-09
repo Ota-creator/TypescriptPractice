292d55a4807ccd620de17725926b49fb7e84a8d4
Ota-creater <bsyota18@gmail.com>
2026-09-09 10:54:56 +0900

    Udemy：複数の型宣言を学習

--- AllPractice/Udemy/src/Session1.ts
+++ AllPractice/Udemy/src/Session1.ts
@@ -12,6 +12,7 @@ let back: string = `hello`;
 let one = 1;
 
 
+//型注釈
 const person: {
     name: string;
     age: number;
@@ -21,9 +22,11 @@ const person: {
 }
 console.log(person.name);
 
+
 //Tuple型
 const book = ["business",1500,false];
 const booka: [string,number,boolean] = ["business",1500,false];
+
 
 //Enum
 enum CoffeSize {
@@ -38,3 +41,42 @@ const coffe = {
     size: CoffeSize.SHORT
 }
 
+
+//union型、複数の型を持たせたい場合 | を用いて接続、or演算子的
+let unionType : number | string = 10;
+unionType = "happy";
+unionType = 20;
+
+//配列指定も可能
+let unionArray: (number | string)[] = [20,"happy"]
+
+
+//リテラル型　決められた型のみを使用
+const banana:"banana" ="banana"
+const lemon:"lemon" ="lemon" //決められた型（lemon）以外代入できない
+
+/*unionとリテラルの混合、enumと似ている
+enumは宣言時にデータを作成するのに対し、組み合わせでは型の指定のみ
+enumはデータを作成するので厳密に扱う必要あり*/
+let clothSize: "small" | "medium" | "large" = "large";
+const cloth:{
+    color: string;
+    size: "small" | "medium" | "large" //混合
+} = {
+    color: "black",
+    size: "large" //事前に指定したsizeから選択
+}
+
+
+//type 新しい型を宣言できる　既に設定されている型は利用できない（number）など
+
+
+//関数に型をつける
+function add (num1:/*ここでパラメータに対して型指定*/　number, num2:number) 
+    :/*ここで戻り値に対して型指定*/ number {
+        return num1 + num2
+}
+add(3,2); //関数は呼び出しを行わなければ機能しない 関数(引数)で宣言
+
+
+//undefinedとnull型は、undefidedとnullしか代入できない
